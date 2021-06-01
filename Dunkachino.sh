439d06152a0197c6ec681e8a70d123eb00eae444
JakobeBussey <58497547+JakobeBussey@users.noreply.github.com>
2021-06-01 14:23:59 -0400

    Add files via upload

---
+++ Dunkachino.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+name="Dunkachino"
+blend="Chocolate"
+
+echo "What's my name?"
+
+echo "$name!"
+
+echo "It's a whole new game!"
+
+echo "$name!"
+
+echo "You want creamy goodness? I'm your friend!"
+
+echo "Say hello to my $blend blend!"
+
+unset name
+unset blend
